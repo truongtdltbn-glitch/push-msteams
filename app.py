da2ac55d0ed30d18ae6303f425ae705341a3c5a7
Admin <admin@gmail.com>
2026-07-24 13:38:08 +0700

    update cccc

--- app.py
+++ app.py
@@ -38,7 +38,9 @@ WEBHOOKS = {
     "esb-channel": "https://defaultc756d8b934af408bb1e49f084cbcc0.23.environment.api.powerplatform.com:443/powerautomate/automations/direct/cu/19/workflows/a8b05568223541de8ffa5bdb5f677dd8/triggers/manual/paths/invoke?api-version=1&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=1lhOSwxUaE5A8LjOWdOMBd4jbJOrhYyIwqQQeE0wl7k",
     "mcc-channel": "https://defaultc756d8b934af408bb1e49f084cbcc0.23.environment.api.powerplatform.com:443/powerautomate/automations/direct/cu/06/workflows/80e45273d7e24161ac2310bf6b4acaac/triggers/manual/paths/invoke?api-version=1&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=-Emw2jJlp4tlsdtNEXiMHCq2gJ_-6DXD8XrbkrduZV8",
     "linux-channel": "https://defaultc756d8b934af408bb1e49f084cbcc0.23.environment.api.powerplatform.com:443/powerautomate/automations/direct/workflows/78321822916845bb9017b1abf4aed664/triggers/manual/paths/invoke?api-version=1&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=tMvvsGjENx2jVUkEMT_IpSSdqRoYYvqroBIapniM_PI",
-    "esign-channel": "https://defaultc756d8b934af408bb1e49f084cbcc0.23.environment.api.powerplatform.com:443/powerautomate/automations/direct/cu/26/workflows/91e7c2b8702246fe8c81be9b68bf491b/triggers/manual/paths/invoke?api-version=1&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=AWGNq51NHm5Gh3gA7YfL1LceRW6Eaparxc7PgZZr3CI"
+    "esign-channel": "https://defaultc756d8b934af408bb1e49f084cbcc0.23.environment.api.powerplatform.com:443/powerautomate/automations/direct/cu/26/workflows/91e7c2b8702246fe8c81be9b68bf491b/triggers/manual/paths/invoke?api-version=1&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=AWGNq51NHm5Gh3gA7YfL1LceRW6Eaparxc7PgZZr3CI",
+    "ocp-channel": "https://defaultc756d8b934af408bb1e49f084cbcc0.23.environment.api.powerplatform.com:443/powerautomate/automations/direct/workflows/cf3e384e35854fee9068eb100fe8230b/triggers/manual/paths/invoke?api-version=1&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=_m-NiI94RaUTucVGzF41tF__bGWwi_4BCDMs2zUWPbE"
+
 }
 
 # ===== Alert / Job level config =====
